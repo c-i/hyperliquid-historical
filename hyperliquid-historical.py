fd1f84e1b9212a602b87f15154bb88f8fb6fc51e
c <chuckiewaitt@gmail.com>
2024-06-19 04:11:45 -0400

    added subparsers

--- hyperliquid-historical.py
+++ hyperliquid-historical.py
@@ -5,6 +5,8 @@ import os
 import argparse
 from datetime import datetime, timedelta
 import asyncio
+import lz4.frame
+
 
 
 
@@ -14,16 +16,24 @@ BUCKET = "hyperliquid-archive"
 # s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 # s3.download_file('hyperliquid-archive', 'market_data/20230916/9/l2Book/SOL.lz4', f"{dir_path}/SOL.lz4")
 
+# earliest date: 20230415/0/
+
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Retrieve historical market data from Hyperliquid exchange")
+    parser = argparse.ArgumentParser(description="Retrieve historical tick level market data from Hyperliquid exchange")
+    subparser = parser.add_subparsers(dest="tool", required=True, help="tool: download, decompress, to_csv")
 
-    parser.add_argument("start_date", help="Starting date as one unbroken string formatted: YYYYMMDD.  e.g. 20230916")
-    parser.add_argument("start_hour", help="Hour of the day as an integer between 0 and 23. e.g. 9")
-    parser.add_argument("end_date", help="Ending date as one unbroken string formatted: YYYYMMDD.  e.g. 20230916")
-    parser.add_argument("end_hour", help="Hour of the day as an integer between 0 and 23. e.g. 9")
-    parser.add_argument("tickers", help="Tickers of assets to be downloaded seperated by spaces. e.g. BTC ETH", nargs="+")
+    global_parser = subparser.add_parser("global_settings", add_help=False)
+    global_parser.add_argument("--all", help="Apply action to all available dates and times.", action="store_true", default=False)
+    global_parser.add_argument("-sd", help="Starting date as one unbroken string formatted: YYYYMMDD.  e.g. 20230916")
+    global_parser.add_argument("-sh", help="Hour of the starting day as an integer between 0 and 23. e.g. 9", type=int, default=0)
+    global_parser.add_argument("-ed", help="Ending date as one unbroken string formatted: YYYYMMDD.  e.g. 20230916")
+    global_parser.add_argument("-eh", help="Hour of the ending day as an integer between 0 and 23. e.g. 9", type=int, default=23)
+    global_parser.add_argument("-t", help="Tickers of assets to be downloaded seperated by spaces. e.g. BTC ETH", nargs="+")
+
+    download_parser = subparser.add_parser("download", help="Download historical market data", parents=[global_parser])
+    decompress_parser = subparser.add_parser("decompress", help="Decompress downloaded lz4 data", parents=[global_parser])
 
 
     return parser.parse_args()
@@ -47,17 +57,17 @@ def make_date_list(start_date, end_date):
 
 
 
-def make_date_hour_list(date_list, start_hour, end_hour):
+def make_date_hour_list(date_list, start_hour, end_hour, delimiter="/"):
     date_hour_list = []
     end_date = date_list[-1]
-    hour = int(start_hour)
+    hour = start_hour
     end = 23
     for date in date_list:
         if date == end_date:
-            end = int(end_hour)
+            end = end_hour
 
         while hour <= end:
-            date_hour = date + "/" + str(hour)
+            date_hour = date + delimiter + str(hour)
             date_hour_list.append(date_hour)
             hour += 1
 
@@ -82,6 +92,13 @@ async def download_objects(s3, assets, date_hour_list):
 
 
 
+
+def decompress_files(assets, date_hour_list):
+    pass
+
+
+
+
 def main():
     s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
     args = get_args()
@@ -89,18 +106,24 @@ def main():
     if not os.path.isdir("downloads"):
         os.mkdir("downloads")
 
-    for asset in args.tickers:
+    for asset in args.t:
         if not os.path.isdir(f"downloads/{asset}"):
             os.mkdir(f"downloads/{asset}")
 
-    date_list = make_date_list(args.start_date, args.end_date)
-    date_hour_list = make_date_hour_list(date_list, args.start_hour, args.end_hour)
-
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(download_objects(s3, args.tickers, date_hour_list))
-    loop.close()
+    date_list = make_date_list(args.sd, args.ed)
     
-    print("Done")
+    if args.tool == "download":
+        date_hour_list = make_date_hour_list(date_list, args.sh, args.eh)
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(download_objects(s3, args.t, date_hour_list))
+        loop.close()
+        
+        print("Done")
+
+    if args.tool == "decompress":
+        date_hour_list = make_date_hour_list(date_list, args.sh, args.eh, delimiter="-")
+
+    
     
 
 
